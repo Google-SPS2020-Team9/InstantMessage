8e154bf3a65d2c2ee62cc7daa196f6dd55c8cf68
Gogo <gogo81745.moe@gmail.com>
2020-08-09 20:22:53 +0800

    add: gradle project for the backend

---
+++ backend/src/main/java/Application.java
@@ -0,0 +1,29 @@
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.ServerWebSocket;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+
+public class Application extends AbstractVerticle {
+
+    @Override
+    public void start() {
+
+        HttpServer server = vertx.createHttpServer();
+
+        Router router = Router.router(vertx);
+        router.route("/echo").handler((RoutingContext ctx) -> {
+
+            ServerWebSocket socket = ctx.request().upgrade();
+
+            socket.textMessageHandler(text -> {
+                System.out.println(text);
+                socket.writeTextMessage(text);
+            });
+
+        });
+
+        server.requestHandler(router).listen(8080);
+    }
+
+}
---
+++ backend/src/main/java/Main.java
@@ -0,0 +1,15 @@
+import io.vertx.core.Vertx;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Vertx vertx = Vertx.vertx();
+        vertx.deployVerticle(new Application(), result -> {
+            if (result.failed()) {
+                result.cause().printStackTrace();
+                vertx.close();
+            }
+        });
+    }
+
+}
